d0888e3cceed9df493c9d0b922b8f3caa2a524a9
immobarok <immobarokali2001@gmail.com>
2025-11-04 04:11:11 +0600

    update about

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <main id="about" className="min-h-screen w-full">
-      <h1>About Page</h1>
+      <p>(About)</p>
       <h1 className="text-4xl font-bold mt-20 text-center max-w-3xl mx-auto">
         We combine years of web design and branding expertise to craft
         meaningful, story-driven experiences
