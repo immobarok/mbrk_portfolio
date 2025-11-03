35b276e3d40d36252426f4ac7eaad3146d160d67
immobarok <immobarokali2001@gmail.com>
2025-11-04 03:40:50 +0600

    updated about

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -2,6 +2,7 @@ const About = () => {
   return (
     <main id="about" className="min-h-screen w-full">
       <h1>About Page</h1>
+      <p>This is mobarok from bangladesh</p>
     </main>
   );
 };
