1874e0be3703743fc1ea1afa417361d1f47470f1
immobarok <immobarokali2001@gmail.com>
2025-11-04 03:47:47 +0600

    modified

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -3,6 +3,7 @@ const About = () => {
     <main id="about" className="min-h-screen w-full">
       <h1>About Page</h1>
       <p>This is mobarok from bangladesh</p>
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur laboriosam corrupti perferendis provident fugit. Reprehenderit omnis necessitatibus amet aliquid adipisci esse voluptates itaque ratione neque qui minus, quia rerum quisquam maxime sint cumque eius. Aliquam consequatur cumque atque repellendus mollitia assumenda recusandae temporibus cupiditate ut dolor magni natus laudantium eaque, accusamus numquam aperiam perspiciatis doloremque deserunt obcaecati, esse accusantium harum voluptas modi blanditiis. Suscipit voluptates soluta numquam blanditiis voluptatem. Fugiat dolor laudantium, necessitatibus inventore sunt fuga, voluptatibus eligendi nesciunt aspernatur eius modi libero ab officiis unde nobis aut, consectetur omnis accusamus nihil exercitationem illo enim atque tempora maiores! Voluptatibus, impedit.</h1>
     </main>
   );
 };
